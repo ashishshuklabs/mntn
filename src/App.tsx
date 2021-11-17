9fe4ee047f92f83a9cfe28f41366c4af753449b0
Ashish Shukla <ashish.shukla@aderant.com>
2021-11-17 18:19:22 +1300

    removed unwanted code.Barring the parallax scroll effect everything else works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Landing } from "./views";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <Landing />;
 }
 
